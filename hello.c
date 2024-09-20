54df12813066198780ef9472f25ed609410e461d
B2111793 <haB2111793@student.ctu.edu.vn>
2024-09-20 16:37:22 +0700

    add for

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello world!\n");
+    // printf("Hello world!\n");
+    int cnt = 10, index;
+    for(index = 1; index <= cnt; index++){
+        printf("%d ", index);
+    }
     return 0;
 }
